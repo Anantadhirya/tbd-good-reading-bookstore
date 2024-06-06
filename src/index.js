3cf964dd6125f6476a54610810a647add9f99dbc
Anantadhirya <anantadhirya@gmail.com>
2024-06-06 14:34:35 +0700

    feat: connect db

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import "dotenv/config";
 import express from "express";
+import db from "./db/index.js";
 
 const app = express();
+
+app.get("/", async (req, res) => {
+  const query = `SELECT * FROM "Book"`;
+  const result = await db.query(query);
+  res.send(result.rows);
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Running on port ${port}`));
