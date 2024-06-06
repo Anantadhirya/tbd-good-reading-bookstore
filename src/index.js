49079c5df1d7b1d899d45602118a3ac2c35d4711
Anantadhirya <anantadhirya@gmail.com>
2024-06-06 14:32:50 +0700

    init: dotenv

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+import "dotenv/config";
 import express from "express";
 
 const app = express();
 
-app.listen(3000, () => console.log("Running on port 3000"));
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Running on port ${port}`));
 
 export default app;
